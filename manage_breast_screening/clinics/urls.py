a2b42cf8d6a153206f5334787e2e79d02df54402
Malcolm Baig <git@baig.org>
2025-06-10 14:23:15 +0100

    Update URLs for clinic page
    Specify explicit urls for the filters, as the previous implementation
    would treat a UUID as a filter rather than the database ID of a clinic.

--- manage_breast_screening/clinics/urls.py
+++ manage_breast_screening/clinics/urls.py
@@ -9,5 +9,19 @@ urlpatterns = [
     # /clinics/{today,upcoming,completed,all}
     # /clinics/{id}
     path("", views.clinic_list, name="index"),
-    path("<str:filter>/", views.clinic_list, name="index_with_filter"),
+    path("today/", views.clinic_list, name="index_today", kwargs={"filter": "today"}),
+    path(
+        "upcoming/",
+        views.clinic_list,
+        name="index_upcoming",
+        kwargs={"filter": "upcoming"},
+    ),
+    path(
+        "completed/",
+        views.clinic_list,
+        name="index_completed",
+        kwargs={"filter": "completed"},
+    ),
+    path("all/", views.clinic_list, name="index_all", kwargs={"filter": "all"}),
+    path("<uuid:id>/", views.clinic, name="show"),
 ]
