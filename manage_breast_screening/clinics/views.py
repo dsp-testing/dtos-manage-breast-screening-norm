f9d882d9a5fbdfe5ef814d814ac0542e7e7ef9b6
Mat Moore <MatMoore@users.noreply.github.com>
2025-06-23 16:49:38 +0100

    perf: prefetch clinics
    This is a workaround to avoid an N+1 issue due to the
    AppointmentPresenter calling ClinicSlotPresenter, which calls
    clinic_slot.clinic.

--- manage_breast_screening/clinics/views.py
+++ manage_breast_screening/clinics/views.py
@@ -22,7 +22,7 @@ def clinic(request, id, filter="remaining"):
     presented_clinic = ClinicPresenter(clinic)
     appointments = (
         Appointment.objects.for_clinic_and_filter(clinic, filter)
-        .select_related("clinic_slot", "screening_episode__participant")
+        .select_related("clinic_slot__clinic", "screening_episode__participant")
         .order_by("clinic_slot__starts_at")
     )
     counts_by_filter = Appointment.objects.filter_counts_for_clinic(clinic)
