523e1f066a72bae6e32503cc32897e549b68a351
Steve Laing <steve.laing@gmail.com>
2025-06-24 15:13:47 +0100

    Initial models for Notifications
    Models for message batches and individual messages.
    Adding Appointment and Clinic models separate to other app models.
    Omitting any status related models for now.

--- manage_breast_screening/notifications/models.py
+++ manage_breast_screening/notifications/models.py
@@ -1,1 +1,113 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+
+from ..core.models import BaseModel
+
+BATCH_STATUSES = [
+    ("unscheduled", "Unscheduled"),
+    ("scheduled", "Scheduled"),
+    ("sent", "Sent"),
+    ("failed", "Failed"),
+]
+
+MESSAGE_STATUSES = [
+    ("pending_enrichment", "Pending enrichment"),
+    ("enriched", "Enriched"),
+    ("sending", "Sending"),
+    ("delivered", "Delivered"),
+    ("failed", "Failed"),
+]
+
+
+class MessageBatch(BaseModel):
+    """
+    Multiple messages sent as a batch
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    notify_id = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    sent_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=50, choices=BATCH_STATUSES, default="unscheduled"
+    )
+
+    def __str__(self):
+        return f"MessageBatch {self.id} - Status: {self.status}"
+
+
+class Message(models.Model):
+    """
+    A message sent to a participant.
+    This is usually linked to a MessageBatch but can be a standalone message.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    notify_id = models.CharField(max_length=50, blank=True)
+    batch = models.ForeignKey(
+        MessageBatch,
+        related_name="messages",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(null=True, blank=True)
+    sent_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=50, choices=MESSAGE_STATUSES, default="pending_enrichment"
+    )
+
+    appointment = models.ForeignKey(
+        "notifications.Appointment", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return f"Message about {self.appointment} - Sent at: {self.sent_at}"
+
+
+class Appointment(models.Model):
+    """
+    The screening appointment used to build the message.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nbss_id = models.CharField(max_length=30)
+    nhs_number = models.IntegerField(null=False)
+    status = models.CharField(max_length=50)
+    booked_by = models.CharField(max_length=50)
+    cancelled_by = models.CharField(max_length=50)
+    number = models.IntegerField(null=True, default=1)
+    starts_at = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(null=False)
+
+    clinic = models.ForeignKey("notifications.Clinic", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Appointment for {self.starts_at} at {self.clinic}"
+
+
+class Clinic(models.Model):
+    """
+    A clinic where an appointment is held.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    alt_name = models.CharField(max_length=50)
+    holding_clinic = models.BooleanField()
+    location_code = models.CharField(max_length=50)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50)
+    address_line_3 = models.CharField(max_length=50)
+    address_line_4 = models.CharField(max_length=50)
+    address_line_5 = models.CharField(max_length=50)
+    address_line_5 = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=50)
+    created_at = models.DateTimeField(null=False)
+    updated_at = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return f"Clinic {self.name} ({self.code})"
